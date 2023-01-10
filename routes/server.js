5abd3ac00fc709c954c75e18d945896407fefdb9
Carson Brown <carsonbrown47@gmail.com>
2023-01-10 15:53:17 -0500

    Added email reminder at 10pm (22:00) to make or update daily log post

--- routes/server.js
+++ routes/server.js
@@ -2,36 +2,60 @@ const express = require("express");
 const cron = require('node-cron');
 const nodemailer = require('nodemailer');
 const router = express.Router();
-const { SERVER_STATUS, POST_TYPES } = require("../util/constants");
+const { SERVER_STATUS, POST_TYPES, env } = require("../util/constants");
 const { DailyLog } = require("../models/post");
+const User = require("../models/user");
 
 router.get("/health", (req, res, next) => {
 	res.json({ status: SERVER_STATUS.UP });
 });
 
-// const mailOptions = {
-// 	host: "smtp.mailtrap.io",
-//   port: 2525,
-//   auth: {
-//     user: env.MAIL_USER,
-//     pass: env.MAIL_PASS,
-//   }
-// };
+const mailOptions = {
+	host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: env.MAIL_USER,
+    pass: env.MAIL_PASS,
+  }
+};
 
-// var transport = nodemailer.createTransport(mailOptions, function (err, info) {
-// 	if(err)
-// 		console.log(err);
-// 	else
-// 		console.log(info);
-// });
+var transport = nodemailer.createTransport(mailOptions, function (err, info) {
+	if(err)
+		console.log(err);
+	else
+		console.log(info);
+});
 
-// transport.verify(function (error, success) {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log("Server is ready to take our messages");
-//   }
-// });
+const dailyLogReminder = cron.schedule('0 0 22 * * 0-6', () => {
+	console.log("Reminding user to make or update daily log");
+	User.find()
+		.then(users => {
+			if(!users.length > 0) {
+				const error = new Error('Could not retrieve users.');
+				error.statusCode = 500;
+				throw error;
+			}
+
+			users.forEach(user => {
+				if (!user.email.length > 0) {
+					return;
+				}
+
+				transport.sendMail({
+					to: user.email,
+					from: "no-reply@api.com",
+					subject: "Today's daily log posting.",
+					html: `<h1>Today's daily log will be unavailable to be edited at midnight!</h1>`
+				});
+			})
+		})
+		.catch(err => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
+	});
 
 const updatePostEditability = cron.schedule('59 59 23 * * 0-6', () => {
 	console.log("Updating post editability");
