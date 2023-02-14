b9da5385c0047e672687e2d24c2fb3c2c1d8563c
Carson Brown <carsonbrown47@gmail.com>
2023-02-14 18:51:04 -0500

    changed model to encorporate nextDate, endDate, and task duration for repeating tasks

--- models/task.js
+++ models/task.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { TASK_FREQUENCIES } = require('../util/constants')
+const dateFns = require('date-fns');
  
 const taskSchema = new Schema({
   name: {
@@ -25,11 +27,53 @@ const taskSchema = new Schema({
     type: Date,
     required: true,
   },
+  nextDate: {
+    type: Date,
+    required: false,
+  },
+  duration: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 2,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
   author: {
     type: Schema.Types.ObjectId,
     ref: 'User',
     required: true,
   },
 });
+
+taskSchema.statics.getNextActiveDate = (date, frequency) => {
+  switch (frequency[1]) {
+    case TASK_FREQUENCIES.DAY:
+      return dateFns.addDays(date, parseInt(frequency[0])).setUTCHours(0, 0, 0, 0);;
+    case TASK_FREQUENCIES.WEEK:
+      return dateFns.addDays(date, parseInt(frequency[0]) * 7).setUTCHours(0, 0, 0, 0);;
+    default:
+      const error = new Error('invalid frequency.');
+      error.statusCode = 500;
+      throw error;
+  }
+}
+
+taskSchema.statics.getEndDate = (date, duration) => {
+  switch (duration[1]) {
+    case TASK_FREQUENCIES.DAY:
+      var endDate = dateFns.addDays(date, parseInt(duration[0]) - 1).setUTCHours(23, 59, 59, 999);
+      return endDate
+    case TASK_FREQUENCIES.WEEK:
+      var endDate = dateFns.addDays(date, parseInt(duration[0]) * 6).setUTCHours(23, 59, 59, 999);
+      return endDate
+    default:
+      const error = new Error('invalid duration.');
+      error.statusCode = 500;
+      throw error;
+  }
+}
 
 module.exports = mongoose.model("tasks", taskSchema);
