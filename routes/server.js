2b5f92e869612af70681a7696ddbcf7d827924b2
Carson Brown <carsonbrown47@gmail.com>
2023-02-14 18:52:48 -0500

    added cron scheduled job to update a task available to be displayed again (repeat)

--- routes/server.js
+++ routes/server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const cron = require('node-cron');
 const nodemailer = require('nodemailer');
 const router = express.Router();
-const { SERVER_STATUS, POST_TYPES, env } = require("../util/constants");
+const { SERVER_STATUS, POST_TYPES, TASK_STATUS, env } = require("../util/constants");
 const { DailyLog } = require("../models/post");
+const Task = require('../models/task');
 const User = require("../models/user");
 
 router.get("/health", (req, res, next) => {
@@ -77,7 +78,72 @@ const updatePostEditability = cron.schedule('59 59 23 * * 0-6', () => {
 		]}, updateDoc)
 	.catch(err => {
 		console.log(err);
-	})
+	});
 });
+
+const enableTasks = cron.schedule('0 0 0 * * 0-6', async() => {
+	console.log("Enabling today's tasks");
+	const date = new Date().setUTCHours(0, 0, 0, 0);
+
+	const tasks = await Task.find({
+		$and: [
+			{nextDate: {$gte: date }},
+			{endDate: {$lt: date }},
+		]
+	});
+
+	for (const task of tasks) {
+		const nextDate = Task.getNextActiveDate(date, task.frequency);
+		const endDate = Task.getEndDate(date, task.duration);
+
+		const updateDoc = {
+			$set: {
+				status: TASK_STATUS.INCOMPLETE,
+				date: date,
+				nextDate: nextDate,
+				endDate: endDate,
+			}
+		};
+
+		try {
+			await Task.updateOne({_id: task._id}, updateDoc);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+});
+
+const test = async() => {
+	const date = new Date().setUTCHours(0, 0, 0, 0);
+
+	const tasks = await Task.find({
+		$and: [
+			{nextDate: {$gte: date }},
+			{endDate: {$lt: date }},
+		]
+	});
+
+	for (const task of tasks) {
+		const nextDate = Task.getNextActiveDate(date, task.frequency);
+		const endDate = Task.getEndDate(date, task.duration);
+
+		const updateDoc = {
+			$set: {
+				status: TASK_STATUS.INCOMPLETE,
+				date: date,
+				nextDate: nextDate,
+				endDate: endDate,
+			}
+		};
+
+		try {
+			await Task.updateOne({_id: task._id}, updateDoc);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+}
+
+test();
 
 module.exports = router;
