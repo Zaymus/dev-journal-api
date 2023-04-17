2caf978edde8443e15eace5d40bf3179e7d42e4b
Carson Brown <carsonbrown47@gmail.com>
2023-04-17 16:44:00 -0400

    CORS testing

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.use(express.json());
 app.use(function (req, res, next) {
 	const origin = req.headers.origin;
 	if(allowedOrigins.includes(origin)) {
-		res.setHeader('Access-Control-Allow-Origin', origin);
 	}
+	res.setHeader('Access-Control-Allow-Origin', "http://localhost:3000");
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
 	next();
